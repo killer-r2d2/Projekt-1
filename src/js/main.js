5ff1e8295ccbcd999e3bdcfd72db03556adc207e
Roger Killer <roger.killer@advery.ch>
2023-05-20 21:20:00 +0200

    move todo functions to todos-stores.js

--- src/js/main.js
+++ src/js/main.js
@@ -8,46 +8,4 @@ themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
-const createHtmlForTodo = (todo) => {
-  const todoHtml = `
-      <div class="statusAndDurationBox">
-        <div>
-          <input type="checkbox" id="statusCheckbox" />
-          <label for="statusCheckbox">${todo.status ? "Done" : "Open"}</label>
-        </div>
-        <p>${todo.dueDate}</p>
-      </div>
-      <div>
-        <h2>${todo.todo}</h2>
-        <p>
-          ${todo.description}
-        </p>
-      </div>
-      <div>
-        <img
-          class="iconArrowTrendingUp"
-          src="./src/icons/iconTrendingUp.svg"
-          alt="Arrow trending up"
-        />
-      </div>
-      <div>
-        <button id="editTodo" class="primaryButton">Edit</button>
-        <button id="deleteTodo" class="primaryButton">Delete</button>
-      </div>
-  `;
-  return todoHtml;
-};
-
-const renderTodos = () => {
-  const todoList = document.querySelector("#todoList");
-  const df = document.createDocumentFragment();
-  todosStore.todos.forEach((todo) => {
-    const liElement = document.createElement("li");
-    liElement.classList.add("listEntry");
-    liElement.innerHTML = createHtmlForTodo(todo);
-    df.appendChild(liElement);
-  });
-  todoList.appendChild(df);
-};
-
-renderTodos();
+todosStore.renderTodos();
