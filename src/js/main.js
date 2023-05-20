edf838cae181451c3ba9e53f5c77c5bbcc25189e
Roger Killer <roger.killer@advery.ch>
2023-05-20 21:12:46 +0200

    create a todos-stores.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,57 +1,53 @@
 "use strict";
 
+import todosStore from "./todos-stores.js";
+
 // toggle theme
 const themeToggler = document.getElementById("themeToggler");
 themeToggler.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
 
-// get the todos from local storage
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
-console.log(todos);
-const todoList = document.querySelector("#todos");
-
-// render html for each todo with template literals
-const createTodoHtml = (todo) => {
-  const li = `
-  <li class="listEntry">
-    <div class="statusAndDurationBox">
-      <div>
-        <input type="checkbox" id="statusCheckbox" />
-        <label for="statusCheckbox">${todo.status ? "Done" : "Open"}</label>
+const createHtmlForTodo = (todo) => {
+  const todoHtml = `
+      <div class="statusAndDurationBox">
+        <div>
+          <input type="checkbox" id="statusCheckbox" />
+          <label for="statusCheckbox">${todo.status ? "Done" : "Open"}</label>
+        </div>
+        <p>${todo.dueDate}</p>
       </div>
-      <p>${todo.dueDate}</p>
-    </div>
-    <div>
-      <h2>${todo.todo}</h2>
-      <p>
-        ${todo.description}
-      </p>
-    </div>
-    <div>
-      <img
-        class="iconArrowTrendingUp"
-        src="./src/icons/iconTrendingUp.svg"
-        alt="Arrow trending up"
-      />
-    </div>
-    <div>
-      <button id="editTodo" class="primaryButton">Edit</button>
-      <button id="deleteTodo" class="primaryButton">Delete</button>
-    </div>
-  </li>`;
-  return li;
+      <div>
+        <h2>${todo.todo}</h2>
+        <p>
+          ${todo.description}
+        </p>
+      </div>
+      <div>
+        <img
+          class="iconArrowTrendingUp"
+          src="./src/icons/iconTrendingUp.svg"
+          alt="Arrow trending up"
+        />
+      </div>
+      <div>
+        <button id="editTodo" class="primaryButton">Edit</button>
+        <button id="deleteTodo" class="primaryButton">Delete</button>
+      </div>
+  `;
+  return todoHtml;
 };
 
 const renderTodos = () => {
+  const todoList = document.querySelector("#todoList");
   const df = document.createDocumentFragment();
-  todos.forEach((todo) => {
-    const todoHtml = document
-      .createRange()
-      .createContextualFragment(createTodoHtml(todo));
-    df.appendChild(todoHtml); // append li to the document fragment
+  todosStore.todos.forEach((todo) => {
+    const liElement = document.createElement("li");
+    liElement.classList.add("listEntry");
+    liElement.innerHTML = createHtmlForTodo(todo);
+    df.appendChild(liElement);
   });
-  todoList.appendChild(df); // append document fragment to the todoList
+  todoList.appendChild(df);
 };
 
 renderTodos();
---
+++ src/js/todos-stores.js
@@ -0,0 +1,44 @@
+const todos = [
+  {
+    id: 1,
+    description: "This is a description",
+    dueDate: "2021-10-10",
+    importance: "high",
+    status: false,
+    todo: "This is a todo",
+  },
+  {
+    id: 2,
+    description: "This is a description",
+    dueDate: "2021-10-10",
+    importance: "high",
+    status: false,
+    todo: "This is a todo",
+  },
+  {
+    id: 3,
+    description: "This is a description",
+    dueDate: "2021-10-10",
+    importance: "high",
+    status: false,
+    todo: "This is a todo",
+  },
+  {
+    id: 4,
+    description: "This is a description",
+    dueDate: "2021-10-10",
+    importance: "high",
+    status: false,
+    todo: "This is a todo",
+  },
+  {
+    id: 5,
+    description: "This is a description",
+    dueDate: "2021-10-10",
+    importance: "high",
+    status: false,
+    todo: "This is a todo",
+  },
+];
+
+export default { todos };
